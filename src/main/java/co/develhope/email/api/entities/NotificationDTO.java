939617416a35cb3874bff1835408a2b9b36ddb2e
Claudio <claudiodd96@gmail.com>
2022-12-12 19:13:37 +0100

    Initial commit

---
+++ src/main/java/co/develhope/email/api/entities/NotificationDTO.java
@@ -0,0 +1,10 @@
+package co.develhope.email.api.entities;
+
+import lombok.Data;
+
+@Data
+public class NotificationDTO {
+    private String contactId;
+    private String title;
+    private String text;
+}
